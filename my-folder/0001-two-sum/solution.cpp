5b1d9234027a5dbdb18ab73f41ffdc83d7b6fba4
Dynamo_28 <soumyadeep.dev.28@gmail.com>
2026-01-23 17:45:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14.8 MB (42.97%)

--- my-folder/0001-two-sum/solution.cpp
+++ my-folder/0001-two-sum/solution.cpp
@@ -1,26 +1,15 @@
-#include <vector>
-#include <unordered_map>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> numMap;
+      vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> numMap;
         
         for (int i = 0; i < nums.size(); i++) {
             int complement = target - nums[i];
-            
-            if (numMap.count(complement)) {
-                return {numMap.at(complement), i};
+            if (numMap.find(complement) != numMap.end()) {
+                return {numMap[complement], i};
             }
-            
             numMap[nums[i]] = i;
         }
-        
-        // This line is theoretically unreachable based on the problem constraints
-        // ("Only one valid answer exists"), but it's good practice for a non-void function.
         return {};
     }
 };
