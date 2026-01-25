a5d172b8a7ce7beba224c90d7416962b49d9d684
Dynamo_28 <soumyadeep.dev.28@gmail.com>
2026-01-25 05:47:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 22.6 MB (80.68%)

--- my-folder/0026-remove-duplicates-from-sorted-array/solution.cpp
+++ my-folder/0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        std::vector<int> arr;
         int c=0;
-        arr.push_back(nums[0]);
             for(int j=1;j<nums.size();j++)
             {
                 if (nums[j]!=nums[c])
