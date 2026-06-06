de5feb96fad0c466a0c2f69a2bbc80af0b846a1f
Dynamo_28 <soumyadeep.dev.28@gmail.com>
2026-06-06 16:33:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.4 MB (14.78%)

--- my-folder/0125-valid-palindrome/solution.cpp
+++ my-folder/0125-valid-palindrome/solution.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
+    bool palindrome(int i,int n, vector<char> &s1)
+    {
+        if (i>=n/2)
+            return true;
+        if (s1[i]!=s1[n-i-1])
+            return false;
+        return palindrome(i+1,n,s1);
+    }
     bool isPalindrome(string s) {
-        int i,j;
+        int i;
+        vector<char> s2;
         for( i=0;i<s.size();)
         {
             if (s[i]>='A' && s[i]<='Z')
                 s[i]=(char)tolower(s[i]);
-            if (!((s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9')))
-                s.erase(s.begin()+i);
-            else
+            if (((s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9')))
+                s2.push_back(s[i]);
+            
                 i++;
         }
-        for( i=s.size()-1, j=0;i>=0,j<s.size();i--,j++)
-        {
-            if (s[i]!=s[j])
-                return false;
-            if (i==j || (i-j)==1)
-                return true;
-        }
-        return true;
+        return palindrome(0,s2.size(),s2);
     }
+    
 };
+
