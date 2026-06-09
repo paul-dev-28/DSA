c2e71d979e46646662683ebf9abd446b503f4261
Dynamo_28 <soumyadeep.dev.28@gmail.com>
2026-06-09 21:06:16 +0000

    [LeetCode Sync] Runtime - 3 ms (45.05%), Memory - 9.8 MB (49.53%)

--- my-folder/0242-valid-anagram/solution.cpp
+++ my-folder/0242-valid-anagram/solution.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if (s.size()!=t.size())
-            return false;
-        for(int i=0;i<s.size();i++)
+        vector<int> freq(26);
+        for(char c:s)
+            freq[c-'a']++;
+        for(char c:t)
+            freq[c-'a']--;
+        for(int i=0;i<26;i++)
         {
-            if (s[i]!=t[i])
+            if(freq[i]!=0)
                 return false;
         }
         return true;
