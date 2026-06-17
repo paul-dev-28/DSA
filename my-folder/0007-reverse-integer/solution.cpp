03ca7df399e8ed9335b60c29fa782d35be5b7192
Dynamo_28 <soumyadeep.dev.28@gmail.com>
2026-06-17 07:55:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.5 MB (83.55%)

--- my-folder/0007-reverse-integer/solution.cpp
+++ my-folder/0007-reverse-integer/solution.cpp
@@ -1,27 +1,15 @@
-#include<iostream>
-#include<limits>
-using namespace std;
 class Solution {
 public:
-    long int reverse(long int x) {  
-        long int rev=0;
-
-        while(x!=0)
+    int reverse(int x) {
+        long long rev=0LL;
+        long long num=x;
+        while(num!=0)
         {
-            int digit=x%10;
-            x/=10;
-            if ((rev > INT_MAX / 10) || (rev == INT_MAX / 10 && digit > 7)) 
+            rev=rev*10+(num%10);
+            num/=10;
+            if (rev>INT_MAX || rev< INT_MIN)
                 return 0;
-            if ((rev < INT_MIN / 10) || (rev == INT_MIN / 10 && digit < -8)) 
-                return 0;
-                rev=rev*10+digit;
         }
-            return rev;
-    }
-    void main()
-    {
-        long int n;
-        cin >> n;
-        cout << reverse(n);
+        return (int)rev;
     }
 };
