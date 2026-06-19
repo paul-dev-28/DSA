2ce4601d4e9a52e9d31564059fd53366db6e12a1
Dynamo_28 <soumyadeep.dev.28@gmail.com>
2026-06-19 12:37:48 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.6 MB (78.24%)

--- my-folder/0035-search-insert-position/solution.cpp
+++ my-folder/0035-search-insert-position/solution.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int l=0,u=nums.size()-1,mid=0;
+        int ans = u+1;
         while(l<=u)
         {
             mid=(u+l)/2;
-            if (target<nums[mid]){
+            if (target<=nums[mid])
+            {
+                    ans=mid;
                     u=mid-1;
-                    
             }
-                
-            if(target>nums[mid]){
+            else
                 l=mid+1;
-                
-            }
-            if(target==nums[mid])
-                return mid;
-            
         }
-        
-        return l;
+        return ans;
     }
 };
