941002d0f29e635eb227cc2b41942aa0189fd530
Dynamo_28 <soumyadeep.dev.28@gmail.com>
2026-06-20 10:07:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.5 MB (97.42%)

--- my-folder/0162-find-peak-element/solution.cpp
+++ my-folder/0162-find-peak-element/solution.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int n=nums.size();
+        int l=1,h=n-2,mid=0;
         if (n==1)
             return 0;
-        else if(nums[0]>nums[1])
-            return 0;
-        else if(nums[n-1]>nums[n-2])
-            return (n-1);
-        int c=1,k=0;
-        while(c<(n-1))
+        if (nums[0]>nums[1])    return 0;
+        if (nums[n-1]>nums[n-2])    return n-1;
+        while(l<=h)
         {
-            if (nums[c]>nums[c-1] && nums[c]>nums[c+1])
-                k=1;
-            c++;
-            if(k==1){
-                return c-1;
-                break;}
+            mid=l+(h-l)/2;
+            if ((nums[mid]> nums[mid-1]) && (nums[mid]>nums[mid+1]))
+                return mid;
+            else if(nums[mid]>nums[mid-1])
+                l=mid+1;
+            else if(nums[mid]>nums[mid+1])
+                h=mid-1;
+            else
+                l=mid+1;
         }
-        return c-1;
+        return -1;
     }
 };
