69eb102b2c97c37696a67781fbf9f1fc31b28155
Dynamo_28 <soumyadeep.dev.28@gmail.com>
2026-06-29 20:16:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 23.1 MB (20.70%)

--- my-folder/0110-balanced-binary-tree/solution.cpp
+++ my-folder/0110-balanced-binary-tree/solution.cpp
@@ -11,20 +11,23 @@
  */
 class Solution {
 public:
-    bool isBalanced(TreeNode* root) {
-        return checkHeight(root) != -1;
-    }
-
-private:
-    int checkHeight(TreeNode* node) {
-        if (node == nullptr) return 0;
-        int leftHeight = checkHeight(node->left);
-        if (leftHeight == -1) return -1;
-        int rightHeight = checkHeight(node->right);
-        if (rightHeight == -1) return -1;
-        if (abs(leftHeight - rightHeight) > 1) {
+    int dfs(TreeNode* root)
+    {
+        if (root==NULL)
+            return 0;
+        int lh=dfs(root->left);
+        if (lh==-1) 
             return -1;
-        }
-        return max(leftHeight, rightHeight) + 1;
+        int rh=dfs(root->right);
+        if (rh==-1) 
+            return -1;
+        if (abs(lh-rh)>1)   
+            return -1;
+        return 1+max(lh,rh);
+    }
+    bool isBalanced(TreeNode* root) {
+        if (dfs(root)==-1)
+            return false;
+        return true;
     }
 };
