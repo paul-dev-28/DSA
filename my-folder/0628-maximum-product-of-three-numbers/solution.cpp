1b2504cbc91501665a561971926e2126b7ec02ce
Dynamo_28 <soumyadeep.dev.28@gmail.com>
2026-07-26 04:34:57 +0000

    [LeetCode Sync] Runtime - 3 ms (60.93%), Memory - 31.5 MB (78.59%)

--- my-folder/0628-maximum-product-of-three-numbers/solution.cpp
+++ my-folder/0628-maximum-product-of-three-numbers/solution.cpp
@@ -3,6 +3,10 @@ public:
     int maximumProduct(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        return max((nums[n-1]*nums[n-2]*nums[n-3]),(nums[0]*nums[1]*nums[n-1]));
+        int n1=nums[n-1]*nums[n-2]*nums[n-3];
+        int n2=nums[0]*nums[1]*nums[n-1];
+        if (n1>n2)
+            return n1;
+        return n2;
     }
 };
